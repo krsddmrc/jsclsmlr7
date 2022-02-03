9579f42c225e5be33e9299fcce4d46d911530603
k2rdnz <k2rdnz@gmail.com>
2022-02-04 01:09:34 +0300

    yedinci çalışma-arrays eklendi

---
+++ jsarray.js
@@ -0,0 +1,5 @@
+var s =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, eligendi accusamus maxime in dolorum quo? Error, ad nostrum saepe officia mollitia molestias maiores tempora illo consequuntur quidem, pariatur quas ratione corporis rem. Fuga ut ducimus rem porro mollitia maxime, expedita, a ullam magni perspiciatis itaque optio obcaecati corporis eaque velit aut accusantium reprehenderit, iste excepturi ipsam deleniti sunt nulla impedit neque. Architecto rem dolores temporibus dolore. Nisi, perspiciatis voluptas. Molestiae nemo dolore ipsa laboriosam libero earum, ipsum quis dolor explicabo? Dignissimos debitis nam iure veritatis excepturi dolor.";
+/* console.log(s.search('elit'));
+    console.log(s.search(/Text/i)); */
+console.log(s.slice(0, 5));
